5184f5a8ae1082a4e3ad89aaea7706b1476a588b
czn <2985697630@qq.com>
2017-03-03 14:38:29 +0800

    init

---
+++ app/src/main/java/me/yifeiyuan/understandingdatabinding/BindAdapters.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2017, 程序亦非猿
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.yifeiyuan.understandingdatabinding;
+
+import android.databinding.BindingAdapter;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+/**
+ * Created by 程序亦非猿 on 17/2/15.
+ */
+public class BindAdapters {
+
+    private static final String TAG = "BindAdapters";
+
+    /**
+     * 自定义属性 会走到这里处理
+     * @param view
+     * @param url
+     */
+    @BindingAdapter({"imgUrl"})
+    public static void loadImg(ImageView view, String url) {
+        Log.d(TAG, "loadImg() called with: view = [" + view + "], url = [" + url + "]");
+        Glide.with(view.getContext()).load(url).placeholder(R.mipmap.ic_launcher).into(view);
+    }
+}
---
+++ app/src/main/java/me/yifeiyuan/understandingdatabinding/MainActivity.java
@@ -0,0 +1,30 @@
+package me.yifeiyuan.understandingdatabinding;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.yifeiyuan.understandingdatabinding.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActivityMainBinding mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mainBinding.setUser(createUser());
+        mainBinding.setImgUrl("http://img.qqai.net/uploads/i_5_4094424040x1536326957_21.jpg");
+    }
+
+    private User createUser() {
+        User user = new User();
+        user.id = 900110;
+        user.name = "程序亦非猿";
+//        user.nickName = "";// "" 不行 一定要 null 才会显示 name
+        user.age = 18;
+        user.male = true;
+        user.birthDay = System.currentTimeMillis();
+        return user;
+    }
+}
